012b6e30353cca7ab9a287c9e8d3fe135115abe4
Ryo Yamamoto <ryoyamamoto.jp@gmail.com>
2014-01-31 18:40:16 +0900

    Initial commit

---
+++ src/main/java/jp/ryoyamamoto/poiutils/SS.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright 2014 Ryo Yamamoto
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.ryoyamamoto.poiutils;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.AreaReference;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+/**
+ * Utility methods for SS.
+ * 
+ * @author Ryo Yamamoto
+ */
+public class SS {
+
+    private SS() {
+    }
+
+    /*
+     * Public methods
+     */
+    /**
+     * Copies a cell, including formulas and their resulting values, comments,
+     * and cell formats.
+     * 
+     * @param source
+     *            the source cell.
+     * @param target
+     *            the target cell.
+     */
+    public static void copy(Cell source, Cell target) {
+        if (source == null) {
+            target = null;
+            return;
+        }
+
+        copyCellValue(source, target);
+        copyCellStyle(source, target);
+        copyCellComment(source, target);
+        copyHyperlink(source, target);
+    }
+
+    /**
+     * Clears a cell to remove the cell contents (formulas and data), formats
+     * (including number formats, conditional formats, and borders), and any
+     * attached comments.
+     * <p>
+     * The cleared cell remain as a blank or an unformatted cell.
+     * </p>
+     * 
+     * @param cell
+     *            the cell to be cleared.
+     */
+    public static void clear(Cell cell) {
+        if (cell == null) {
+            return;
+        }
+
+        cell.setCellType(Cell.CELL_TYPE_BLANK);
+    }
+
+    /**
+     * Merges two or more adjacent cells.
+     * <p>
+     * Only the data in the upper-left cell of a range will remain in the merged
+     * cell. Data in other cells of the range will be deleted.
+     * </p>
+     * 
+     * @param sheet
+     *            the sheet that the range is on.
+     * @param range
+     *            the range to merge.
+     */
+    public static void merge(Sheet sheet, CellRangeAddress range) {
+        boolean copied = false;
+        Cell upperLeftCell = getCell(sheet, getFirstCellReference(range));
+        for (CellReference reference : getCellReferences(range)) {
+            Cell cell = getCell(sheet, reference);
+            if (copied == false && isNotBlank(cell)) {
+                copy(cell, upperLeftCell);
+                copied = true;
+            }
+            if (cell != upperLeftCell) {
+                clear(cell);
+            }
+        }
+        sheet.addMergedRegion(range);
+    }
+
+    /*
+     * Private methods
+     */
+    private static void copyCellValue(Cell source, Cell target) {
+        switch (source.getCellType()) {
+        case Cell.CELL_TYPE_NUMERIC:
+            target.setCellValue(source.getNumericCellValue());
+            break;
+        case Cell.CELL_TYPE_STRING:
+            target.setCellValue(source.getRichStringCellValue());
+            break;
+        case Cell.CELL_TYPE_FORMULA:
+            target.setCellFormula(source.getCellFormula());
+            break;
+        case Cell.CELL_TYPE_BLANK:
+            break;
+        case Cell.CELL_TYPE_BOOLEAN:
+            target.setCellValue(source.getBooleanCellValue());
+            break;
+        case Cell.CELL_TYPE_ERROR:
+            target.setCellErrorValue(source.getErrorCellValue());
+            break;
+        }
+    }
+
+    private static void copyCellStyle(Cell source, Cell target) {
+        target.setCellStyle(source.getCellStyle());
+    }
+
+    private static void copyCellComment(Cell source, Cell target) {
+        target.setCellComment(source.getCellComment());
+    }
+
+    private static void copyHyperlink(Cell source, Cell target) {
+        if (source.getHyperlink() == null) {
+            removeHyperlink(target);
+        } else {
+            target.setHyperlink(source.getHyperlink());
+        }
+    }
+
+    private static Cell getCell(Sheet sheet, int row, int col) {
+        return sheet.getRow(row).getCell(col);
+    }
+
+    private static Cell getCell(Sheet sheet, CellReference ref) {
+        return getCell(sheet, ref.getRow(), ref.getCol());
+    }
+
+    private static CellReference getFirstCellReference(CellRangeAddress range) {
+        return new CellReference(range.getFirstRow(), range.getFirstColumn());
+    }
+
+    private static CellReference getLastCellReference(CellRangeAddress range) {
+        return new CellReference(range.getLastRow(), range.getLastColumn());
+    }
+
+    private static CellReference[] getCellReferences(CellRangeAddress range) {
+        return toAreaReference(range).getAllReferencedCells();
+    }
+
+    private static AreaReference toAreaReference(CellRangeAddress range) {
+        CellReference topLeft = getFirstCellReference(range);
+        CellReference botRight = getLastCellReference(range);
+        return new AreaReference(topLeft, botRight);
+    }
+
+    private static Sheet getSheet(Cell cell) {
+        return cell.getRow().getSheet();
+    }
+
+    private static boolean isBlank(Cell cell) {
+        return cell == null || cell.getCellType() == Cell.CELL_TYPE_BLANK;
+    }
+
+    private static boolean isNotBlank(Cell cell) {
+        return !isBlank(cell);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void removeHyperlink(Cell cell) {
+        if (cell == null || cell.getHyperlink() == null) {
+            return;
+        }
+
+        try {
+            Class<?> clazz = (cell instanceof XSSFCell) ? XSSFSheet.class
+                    : HSSFSheet.class;
+            Field field = clazz.getDeclaredField("hyperlinks");
+            field.setAccessible(true);
+            Sheet sheet = getSheet(cell);
+            List<Hyperlink> hyperlinks = (List<Hyperlink>) field.get(sheet);
+            hyperlinks.remove(cell.getHyperlink());
+        } catch (NoSuchFieldException | SecurityException
+                | IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/jp/ryoyamamoto/poiutils/SSTest.java
@@ -0,0 +1,377 @@
+package jp.ryoyamamoto.poiutils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.ClientAnchor;
+import org.apache.poi.ss.usermodel.Comment;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Drawing;
+import org.apache.poi.ss.usermodel.FormulaError;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SSTest {
+
+    public static class WhenYouCopyFromANumericCellToAnother {
+
+        private Cell source;
+        private Cell target;
+
+        @Before
+        public void setUp() throws Exception {
+            Sheet sheet = createWorkbook().getSheetAt(0);
+            source = sheet.getRow(0).getCell(0);
+            target = sheet.createRow(1).createCell(0);
+        }
+
+        @Test
+        public void theCellTypeOfTheTargetCellShouldBeNumeric() {
+            SS.copy(source, target);
+            assertThat(target.getCellType()).isEqualTo(Cell.CELL_TYPE_NUMERIC);
+        }
+
+        @Test
+        public void theNumericValueOfTheTargetCellShouldBeReadable() {
+            SS.copy(source, target);
+            assertThat(target.getNumericCellValue()).isEqualTo(1);
+        }
+
+        private Workbook createWorkbook() {
+            Workbook workbook = new XSSFWorkbook();
+            workbook.createSheet().createRow(0).createCell(0).setCellValue(1);
+            return workbook;
+        }
+    }
+
+    public static class WhenYouCopyFromAStringCellToAnother {
+
+        private Cell source;
+        private Cell target;
+
+        @Before
+        public void setUp() throws Exception {
+            Sheet sheet = createWorkbook().getSheetAt(0);
+            source = sheet.getRow(0).getCell(0);
+            target = sheet.createRow(1).createCell(0);
+        }
+
+        @Test
+        public void theCellTypeOfTheTargetCellShouldBeString() {
+            SS.copy(source, target);
+            assertThat(target.getCellType()).isEqualTo(Cell.CELL_TYPE_STRING);
+        }
+
+        @Test
+        public void theStringValueOfTheTargetCellShouldBeReadable() {
+            SS.copy(source, target);
+            assertThat(target.getStringCellValue()).isEqualTo("string");
+        }
+
+        private Workbook createWorkbook() {
+            Workbook workbook = new XSSFWorkbook();
+            workbook.createSheet().createRow(0).createCell(0)
+                    .setCellValue("string");
+            return workbook;
+        }
+    }
+
+    public static class WhenYouCopyFromAFormulaCellToAnother {
+
+        private Cell source;
+        private Cell target;
+
+        @Before
+        public void setUp() throws Exception {
+            Sheet sheet = createWorkbook().getSheetAt(0);
+            source = sheet.getRow(0).getCell(0);
+            target = sheet.createRow(1).createCell(0);
+        }
+
+        @Test
+        public void theCellTypeOfTheTargetCellShouldBeFormula() {
+            SS.copy(source, target);
+            assertThat(target.getCellType()).isEqualTo(Cell.CELL_TYPE_FORMULA);
+        }
+
+        @Test
+        public void theFormulaOfTheTargetCellShouldBeReadable() {
+            SS.copy(source, target);
+            assertThat(target.getCellFormula()).isEqualTo("SUM(B1:C1)");
+        }
+
+        private Workbook createWorkbook() {
+            Workbook workbook = new XSSFWorkbook();
+            workbook.createSheet().createRow(0).createCell(0)
+                    .setCellFormula("SUM(B1:C1)");
+            return workbook;
+        }
+    }
+
+    public static class WhenYouCopyFromABlankCellToAnother {
+
+        private Workbook workbook;
+        private Cell source;
+        private Cell target;
+
+        @Before
+        public void setUp() throws Exception {
+            workbook = createWorkbook();
+            source = workbook.getSheetAt(0).getRow(0).getCell(0);
+            target = workbook.getSheetAt(0).createRow(1).createCell(0);
+        }
+
+        @Test
+        public void theCellTypeOfTheTargetCellShouldBeBlank() {
+            SS.copy(source, target);
+            assertThat(target.getCellType()).isEqualTo(Cell.CELL_TYPE_BLANK);
+        }
+
+        @Test
+        public void theNumericValueOfTheTargetCellShouldBeZero() {
+            SS.copy(source, target);
+            assertThat(target.getNumericCellValue()).isZero();
+        }
+
+        @Test
+        public void theStringValueOfTheTargetCellShouldBeEmpty() {
+            SS.copy(source, target);
+            assertThat(target.getStringCellValue()).isEqualTo("");
+        }
+
+        @Test
+        public void theBooleanValueOfTheTargetCellShouldBeFalse() {
+            SS.copy(source, target);
+            assertThat(target.getBooleanCellValue()).isFalse();
+        }
+
+        @Test
+        public void theStyleOfTheTargetCellShouldBeDefault() {
+            SS.copy(source, target);
+            assertThat(target.getCellStyle()).isEqualTo(
+                    workbook.getCellStyleAt((short) 0));
+        }
+
+        @Test
+        public void theCommentOfTheTargetCellShouldBeNull() {
+            SS.copy(source, target);
+            assertThat(target.getCellComment()).isNull();
+        }
+
+        @Test
+        public void theHyperlinkOfTheTargetCellShouldBeNull() {
+            SS.copy(source, target);
+            assertThat(target.getHyperlink()).isNull();
+        }
+
+        private Workbook createWorkbook() {
+            Workbook workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet();
+            // source
+            sheet.createRow(0).createCell(0);
+            // target
+            CreationHelper factory = workbook.getCreationHelper();
+            ClientAnchor anchor = factory.createClientAnchor();
+            Cell cell = sheet.createRow(1).createCell(0);
+            cell.setCellValue(1);
+            CellStyle style = workbook.createCellStyle();
+            style.setAlignment(CellStyle.ALIGN_CENTER);
+            cell.setCellStyle(style);
+            Drawing drawing = sheet.createDrawingPatriarch();
+            Comment comment = drawing.createCellComment(anchor);
+            comment.setString(factory.createRichTextString("Comment"));
+            cell.setCellComment(comment);
+            Hyperlink link = factory.createHyperlink(Hyperlink.LINK_URL);
+            link.setAddress("https://github.com/RyoYamamotoJP");
+            cell.setHyperlink(link);
+            return workbook;
+        }
+    }
+
+    public static class WhenYouCopyFromABooleanCellToAnother {
+
+        private Cell source;
+        private Cell target;
+
+        @Before
+        public void setUp() throws Exception {
+            Sheet sheet = createWorkbook().getSheetAt(0);
+            source = sheet.getRow(0).getCell(0);
+            target = sheet.createRow(1).createCell(0);
+        }
+
+        @Test
+        public void theCellTypeOfTheTargetCellShouldBeBoolean() {
+            SS.copy(source, target);
+            assertThat(target.getCellType()).isEqualTo(Cell.CELL_TYPE_BOOLEAN);
+        }
+
+        @Test
+        public void theBooleanValueOfTheTargetCellShouldBeReadable() {
+            SS.copy(source, target);
+            assertThat(target.getBooleanCellValue()).isTrue();
+        }
+
+        private Workbook createWorkbook() {
+            Workbook workbook = new XSSFWorkbook();
+            workbook.createSheet().createRow(0).createCell(0)
+                    .setCellValue(true);
+            return workbook;
+        }
+    }
+
+    public static class WhenYouCopyFromAErrorCellToAnother {
+
+        private Cell source;
+        private Cell target;
+
+        @Before
+        public void setUp() throws Exception {
+            Sheet sheet = createWorkbook().getSheetAt(0);
+            source = sheet.getRow(0).getCell(0);
+            target = sheet.createRow(1).createCell(0);
+        }
+
+        @Test
+        public void theCellTypeOfTheTargetCellShouldBeError() {
+            SS.copy(source, target);
+            assertThat(target.getCellType()).isEqualTo(Cell.CELL_TYPE_ERROR);
+        }
+
+        @Test
+        public void theErrorValueOfTheTargetCellShouldBeReadable() {
+            SS.copy(source, target);
+            assertThat(target.getErrorCellValue()).isEqualTo(
+                    FormulaError.DIV0.getCode());
+        }
+
+        private Workbook createWorkbook() {
+            Workbook workbook = new XSSFWorkbook();
+            workbook.createSheet().createRow(0).createCell(0)
+                    .setCellErrorValue(FormulaError.DIV0.getCode());
+            return workbook;
+        }
+    }
+
+    public static class WhenYouCopyFromACellToAnother {
+
+        private Workbook workbook;
+        private Cell source;
+        private Cell target;
+
+        @Before
+        public void setUp() throws Exception {
+            workbook = createWorkbook();
+            source = workbook.getSheetAt(0).getRow(0).getCell(0);
+            target = workbook.getSheetAt(0).createRow(1).createCell(0);
+        }
+
+        @Test
+        public void theStyleOfTheTargetCellShouldNotBeDefault() {
+            SS.copy(source, target);
+            assertThat(target.getCellStyle()).isNotEqualTo(
+                    workbook.getCellStyleAt((short) 0));
+        }
+
+        @Test
+        public void theCommentOfTheTargetCellShouldBeReadable() {
+            SS.copy(source, target);
+            assertThat(target.getCellComment().getString().getString())
+                    .isEqualTo("Comment");
+        }
+
+        @Test
+        public void theHyperlinkOfTheTargetCellShouldBeReadable() {
+            SS.copy(source, target);
+            assertThat(target.getHyperlink().getAddress()).isEqualTo(
+                    "https://github.com/RyoYamamotoJP");
+        }
+
+        private Workbook createWorkbook() {
+            Workbook workbook = new XSSFWorkbook();
+            CreationHelper factory = workbook.getCreationHelper();
+            ClientAnchor anchor = factory.createClientAnchor();
+            Sheet sheet = workbook.createSheet();
+            Cell cell = sheet.createRow(0).createCell(0);
+            cell.setCellValue(1);
+            CellStyle style = workbook.createCellStyle();
+            style.setAlignment(CellStyle.ALIGN_CENTER);
+            cell.setCellStyle(style);
+            Drawing drawing = sheet.createDrawingPatriarch();
+            Comment comment = drawing.createCellComment(anchor);
+            comment.setString(factory.createRichTextString("Comment"));
+            cell.setCellComment(comment);
+            Hyperlink link = factory.createHyperlink(Hyperlink.LINK_URL);
+            link.setAddress("https://github.com/RyoYamamotoJP");
+            cell.setHyperlink(link);
+            return workbook;
+        }
+    }
+
+    public static class WhenYouClearACell {
+
+        private Cell target;
+
+        @Before
+        public void setUp() throws Exception {
+            Sheet sheet = createWorkbook().getSheetAt(0);
+            target = sheet.getRow(0).createCell(0);
+        }
+
+        @Test
+        public void theCellTypeOfTheTargetCellShouldBeBlank() {
+            SS.clear(target);
+            assertThat(target.getCellType()).isEqualTo(Cell.CELL_TYPE_BLANK);
+        }
+
+        private Workbook createWorkbook() {
+            Workbook workbook = new XSSFWorkbook();
+            workbook.createSheet().createRow(0).createCell(0).setCellValue(1);
+            return workbook;
+        }
+    }
+
+    public static class WhenYouMergeTwoOrMoreCells {
+
+        private Sheet sheet;
+        private CellRangeAddress range;
+
+        @Before
+        public void setUp() throws Exception {
+            sheet = createWorkbook().getSheetAt(0);
+            range = new CellRangeAddress(0, 1, 0, 1);
+        }
+
+        @Test
+        public void onlyTheValueInTheUpperLeftCellShouldRemain() {
+            SS.merge(sheet, range);
+            assertThat(sheet.getRow(0).getCell(0).getStringCellValue())
+                    .isEqualTo("A1");
+            assertThat(sheet.getRow(0).getCell(1).getCellType()).isEqualTo(
+                    Cell.CELL_TYPE_BLANK);
+            assertThat(sheet.getRow(1).getCell(0).getCellType()).isEqualTo(
+                    Cell.CELL_TYPE_BLANK);
+            assertThat(sheet.getRow(1).getCell(1).getCellType()).isEqualTo(
+                    Cell.CELL_TYPE_BLANK);
+        }
+
+        private Workbook createWorkbook() {
+            Workbook workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet();
+            Row row1 = sheet.createRow(0);
+            row1.createCell(0).setCellValue("A1");
+            row1.createCell(1).setCellValue("B1");
+            Row row2 = sheet.createRow(1);
+            row2.createCell(0).setCellValue("A2");
+            row2.createCell(1).setCellValue("B2");
+            return workbook;
+        }
+    }
+}
